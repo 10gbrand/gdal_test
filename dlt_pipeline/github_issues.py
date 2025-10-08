6a5deb7c3a0017f71217c83e3427784325d273f4
mate01 <mattias.tengbrand@sveaskog.se>
2025-10-08 12:51:52 +0200

    gitignore

--- dlt_pipeline/github_issues.py
+++ dlt_pipeline/github_issues.py
@@ -8,9 +8,9 @@ response = requests.get(url)
 response.raise_for_status()
 
 pipeline = dlt.pipeline(
-pipeline_name="github_issues",
-destination="duckdb",
-dataset_name="github_data",
+    pipeline_name="github_issues",
+    destination="duckdb",
+    dataset_name="github_data",
 )
 # The response contains a list of issues
 load_info = pipeline.run(response.json(), table_name="issues")
